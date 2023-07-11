48aa516cea9166935e14ebe76657dca81e58edae
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-11 11:49:17 -0700

    Add multiply function to javascript

--- javascript.js
+++ javascript.js
@@ -2,7 +2,10 @@ function add(x, y) {
     return parseInt(x) + parseInt(y);
 }
 
-function subtract(x, y){
+function subtract(x, y) {
     return x - y;
 }
     
+function multiply(x, y){
+    return x * y;
+}
