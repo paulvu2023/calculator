5ddd29c678a2a64f7ff5a4e473f51d46afbda53c
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-11 11:53:55 -0700

    Add operator function to call other functions

--- javascript.js
+++ javascript.js
@@ -2,6 +2,17 @@ let firstNum;
 let operator;
 let secondNum;  
 
+function operate(firstNum, operator, secondNum) {
+    if (operator === '+') {
+        add(firstNum, secondNum);
+    } else if (operator === '-') {
+        subtract(firstNum, secondNum);
+    } else if (operator === '*') {
+        multiply(firstNum, secondNum);
+    } else if (operator === '/') {
+        divide(firstNum, secondNum);
+    }
+}
 
 function add(x, y) {
     return parseInt(x) + parseInt(y);
