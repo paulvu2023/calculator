368ae7748311ca42530494a6f4aa9e84189cc06c
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-12 16:28:43 -0700

    Add function to check if operator was inputted

--- javascript.js
+++ javascript.js
@@ -28,17 +28,32 @@ numbers.forEach((number) => {
 operators.forEach((operator) => {
 
     operator.addEventListener('click', () => {
-        if (operator.textContent === "=") {
-            calculate();
-        } else {
-            display.textContent += operator.textContent;
-        }
+        display.textContent += operator.textContent;
+        checkAndCalculate();
     });
 
 });
 
+function check(){
+    let divisions = display.textContent.match(/÷/g).length;
+    let multiplications = display.textContent.match(/×/g).length;
+    let additions = display.textContent.match(/\+/g).length;
+    let subtractions = display.textContent.match(/-/g).length;
+    let equals = display.textContent.match(/=/g).length;
+    if (divisions >= 1 || multiplications >= 1 || additions >= 1
+    || subtractions >= 1 || equals >= 1) {
+            return false;
+    } else {
+        return true;
+    }
+}
+
 function calculate(){
-    
+    // Check how many of each operator are in the display
+    let operatorIndex;
+    operatorIndex = display.textContent.indexOf('÷');
+    firstNum = display.textContent.slice(0, operatorIndex);
+    secondNum = display.textContent.slice(operatorIndex+1);
 }
 
 function operate(firstNum, operator, secondNum) {
