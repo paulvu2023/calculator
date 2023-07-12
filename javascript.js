651b5004f6c8de5eeb7a2049f83ea0376baee3d7
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-12 13:14:27 -0700

    Make clear button clear display text content'

--- javascript.js
+++ javascript.js
@@ -4,12 +4,16 @@ let secondNum;
 
 const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.num');
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click', () => {
+    display.textContent = '';
+});
 
 numbers.forEach((number) => {
-
     number.addEventListener('click', () => {
         display.textContent += number.textContent;
-    }); //try to make buttons actually display something
+    });
 });
 
 function operate(firstNum, operator, secondNum) {
